d0205034e243f1af0edb526ffc0c7ca039aa344a
Lawrence Bass <lbass01@rams.shepherd.edu>
2014-07-08 13:21:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CampusChipin::Application.config.secret_key_base = 'b32a93ab1343cdfd3082816712f4db0eccfe8b1add4888ce4d79df3b2debe8dda1a87534dac9112e8a4e606404aa81f0653078b81593ce0313ffce4744e66e66'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CampusChipin::Application.config.session_store :cookie_store, key: '_campus_chipin_session'
